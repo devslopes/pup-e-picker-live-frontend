eb30d32bfa50f8f69a4294123312f6493ae5b36f
jjhiggz <jonathan.higger@gmail.com>
2023-07-18 15:33:05 -0600

    changed url

--- src/fetch-calls.tsx
+++ src/fetch-calls.tsx
@@ -1,7 +1,10 @@
 import { z } from "zod";
 import { Dog, dogSchema } from "./types";
 
-export const baseUrl = "http://localhost:4000";
+export const baseUrl =
+  import.meta?.env.NODE_ENV === "production"
+    ? "https://pup-e-picker-live-be.vercel.app"
+    : "http://localhost:4000";
 
 export const getAllDogs = () =>
   fetch(`${baseUrl}/dogs`)
